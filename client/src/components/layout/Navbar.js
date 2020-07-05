e7575210942dc0f7a90e15c20920c922abaed31b
nmili997 <milivojevic97@outlook.com>
2020-07-05 19:13:02 +0200

    setting up redux for state management

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
 class Navbar extends Component {
   render() {
     return (
@@ -14,8 +15,9 @@ class Navbar extends Component {
               className="col s5 brand-logo center black-text"
             >
               <i className="material-icons">code</i>
-              MERN
+              Home
             </Link>
+      
           </div>
         </nav>
       </div>
